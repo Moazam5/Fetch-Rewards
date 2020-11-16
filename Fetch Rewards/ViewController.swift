574869daf91ac25c13e3bdb80f6ec64e362bd767
Moazam <m.moazam29@gmail.com>
2020-11-16 18:12:45 -0500

    Initial Commit

---
+++ Fetch Rewards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch Rewards
+//
+//  Created by Moazam Mir on 11/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
